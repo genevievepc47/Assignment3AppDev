f9c82d74e02661fa4eec741390c76c1d33efcfef
Genevieve <genevievepc47@gmail.com>
2020-02-13 10:29:05 -0500

    added custom spinner and added spinner to color activity

--- app/src/main/java/edu/temple/assignment3appdev/ColorActivity.java
+++ app/src/main/java/edu/temple/assignment3appdev/ColorActivity.java
@@ -2,13 +2,18 @@ package edu.temple.assignment3appdev;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+
 
 public class ColorActivity extends AppCompatActivity {
 
+    String[] colors = {"red", "blue", "green", "black", "gray", "cyan", "magenta", "yellow", "lightgray", "darkgray", "aqua", "fuchsia"}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+        mySpinner.setAdapter(new ColorAdapter(ColorActivity.this, colors));
     }
-    //comment
 }

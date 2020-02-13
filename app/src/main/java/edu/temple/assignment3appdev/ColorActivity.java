2f5ad3047c8a71be3b12a319ebbb92e1c94a5d8a
Genevieve <genevievepc47@gmail.com>
2020-02-13 11:25:58 -0500

    selecting a color and having the layout background color change works

--- app/src/main/java/edu/temple/assignment3appdev/ColorActivity.java
+++ app/src/main/java/edu/temple/assignment3appdev/ColorActivity.java
@@ -1,13 +1,17 @@
 package edu.temple.assignment3appdev;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Spinner;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 
 public class ColorActivity extends AppCompatActivity {
 
-    String[] colors = {"red", "blue", "green", "black", "gray", "cyan", "magenta", "yellow", "lightgray", "darkgray", "aqua", "fuchsia"};
+    String[] colors = {"red", "blue", "green", "gray", "cyan", "magenta", "yellow", "lightgray", "darkgray", "aqua", "fuchsia"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,26 @@ public class ColorActivity extends AppCompatActivity {
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
         mySpinner.setAdapter(new ColorAdapter(ColorActivity.this, colors));
+
+        mySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                findViewById(R.id.mainActivity).setBackgroundColor(Color.parseColor(colors[position]));
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
-}
+
+
+    }
+
+
+
